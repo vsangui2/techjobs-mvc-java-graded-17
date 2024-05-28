7b46bf84b66f3c79a09a99d78d502f522b76861c
Vincent Sanguinetti <vincent.m.sanguinetti@gmail.com>
2024-05-27 20:59:21 -0500

    GA#3 complete

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
 
